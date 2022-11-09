4bdc4b1cc9d517472d39e2e72011656664faf6bb
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-11-09 17:45:56 +0900

    updated

--- backend/server.js
+++ backend/server.js
@@ -18,19 +18,19 @@ app.use('/api/feedback', require('./routes/feedbackRoutes.js'));
 // users
 app.use('/api/auth', require('./routes/authRoutes.js'));
 
-if (process.env.NODE_ENV === 'production') {
-	// production
-	app.use('/', express.static(path.join(__dirname)));
-	app.get('*', (_, res) => {
-		res.sendFile(__dirname + '/index.html');
-	});
-} else {
-	// development
-	app.use('/', express.static(path.join(__dirname, '../frontend')));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
-	});
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	// production
+// 	app.use('/', express.static(path.join(__dirname)));
+// 	app.get('*', (_, res) => {
+// 		res.sendFile(__dirname + '/index.html');
+// 	});
+// } else {
+// development
+app.use('/', express.static(path.join(__dirname, '../frontend')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
+});
+// }
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
